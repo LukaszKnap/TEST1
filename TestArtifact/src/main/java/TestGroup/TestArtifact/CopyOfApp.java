da9d98b5b0c82581bd5342d2bbb8a165b362fa93
LukaszKnap <lukknap@gmail.com>
2014-01-28 14:21:01 +0100

    Changed a file
    Signed-off-by: LukaszKnap <lukknap@gmail.com>

---
+++ TestArtifact/src/main/java/TestGroup/TestArtifact/CopyOfApp.java
@@ -0,0 +1,13 @@
+package TestGroup.TestArtifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class CopyOfApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
